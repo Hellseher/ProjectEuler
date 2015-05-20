546a63e1eec73eaf2837a036c45feffcd6999c31
Sharlatan <sharlatan@localhost.localdomain>
2015-05-20 23:57:25 +0100

    _e pe.py Added more  and funcions discriptions. 	new file:   __init__.py 	modified:   pe.py

--- pe.py
+++ pe.py
@@ -2,23 +2,46 @@
 # _*_ coding: UTF-8 _*_
 # File          :  pe.py
 # Created       :  Mon 27 Apr 2015 19:01:22
-# Last Modified :  Sat 16 May 2015 12:32:01
-# Maintainer    :  sharlatan, <sharlatanus@gmail.com>
-# Title         :  PROJECT EULERS FUNCTIONS
+# Last Modified :  Wed 20 May 2015 23:56:34
+# Maintainer    :  sharlatan, <ssharlatanus@gmail.com>
+# Title         :  PROJECT EULER FUNCTIONS
 # License       :  Same as Python (GPL)
 # Credits       :  https://projecteuler.net
 #
 # -=:[ Description ]:=-
 # This file contains functions which I used for solving Project Euler's
 # problems.
+# num is alway a integer number given as argument to any of functions.
+
+def fnls():
+    """ Return a list of used functions. """
+    function_list = {
+        "isp(num)"    : "Check num for primality.",
+        "ps(num)"     : "Yield prime sieve up to num.",
+        "divs(num)"   : "Yield list of proper divisor of num.",
+        "ds(num)"     : "Recursively return digits sum of num.",
+        "dfs(num)"    : "Return factorised digits sum of num.",
+        "dsfs(num)"   : "Return digit sum of factorised digit sum.",
+        "pclrow(num)" : "Yield the n'th row of Pascal triangle.",
+        "trgn(num)"   : "Yield a list of integer triangular numbers up to num.",
+        "ttrn(n)"     : "Yield a list of int tetrahedral nums up to n's pos.",
+        "sqrn(n)"     : "Yield a list of int square nums up to n's pos.",
+        "pntn(num)"   : "Yield a list of int pentagoanl nums up to n's pos."
+                    }
+    for key in function_list:
+        print key,"\t:", function_list[key]
+
+
 # <END  OF  DESCRIPTION>-------------------------------------------------------
 
 
 import math
+import itertools
 
 
+# -=:[ Prime numbers ]:=-
 def isp(num):
-    """ Check num for primirity. """
+    """ Check num for primality. """
     if num == 1:
         return False
     elif num < 4:
@@ -42,16 +65,25 @@ def isp(num):
 
 
 def ps(num):
-    """ Yield prime sieve up to sqrt(num). """
+    """ Yield prime sieve up to num. """
     if num == 2:
         yield 2
-    for i in xrange(1, num):
+    for i in xrange(3, num+1):
         if isp(i):
             yield i
+# <END OF PRIME NUMBRS>--------------------------------------------------------
+
+
+def divs(num):
+    """ Yield list of proper divisor of num. """
+    for d in xrange(1, int(math.sqrt(num))):
+        if num % d == 0:
+            yield d
+            yield num // d
 
 
 def ds(num):
-    """ Return digits sum of num. """
+    """ Recursively return digits sum of num. """
     if num < 10:
         return num
     return num % 10 + ds(num // 10)
@@ -74,18 +106,39 @@ def pclrow(n):
     pos = 1
     yield pos
     for k in xrange(1, n):
-        pos = pos * (n + 1 - k)/k
+        pos = pos * (n+1 - k) / k
         yield pos
     yield 1
 
-# -=:[ Figurate numbers ]:=----------------------------------------------------
 
-def triangn(num):
-    """ Yield a list of ingeter triangular number up to num. """
-    for tr in xrange(0, num):
+# -=:[ Figurate numbers ]:=-
+def trgn(n):
+    """ Yield a list of ingeter triangular numbers up to num. """
+    for tr in xrange(0, n+1):
         yield tr*(tr+1)/2
     # -=:[ Reference ]:=-
     # Carl Friedrich Gauss
     # OEIS : A000217
     # -=:[ Links ]:=-
     # https://en.wikipedia.org/wiki/Triangular_number
+
+
+def ttrn(n):
+    """ Yield a list of integer tetrahedral numbers up to n's position. """
+    for n in xrange(0, n+1):
+        yield (n*(n + 1) * (n + 2)) / 6
+
+
+def sqrn(n):
+    """ Yield a list of nterger square number up to num's position. """
+    for sq in xrange(0, n+1):
+        yield sq**2
+
+
+def pntn(num):
+    """ Yield a list of integer pentagoanl numbers up to num's position. """
+    for pen in xrange(0, num+1):
+        yield (3 * pen**2 - pen) / 2
+    # -=:[ References ]:=-
+    # Euler's theory of partions
+# <END OF FIGURATE NUBERS>-----------------------------------------------------
