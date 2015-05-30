9a488301800e2d73d5ea8e0d1f13f2e4076a2b40
Sharlatan <sharlatanus@gmail.com>
2015-05-30 23:35:42 +0100

    Tried to dig into __init__.py and __all__ ...
    _a:_e some function for polygonal numbers (figurative).
    	modified:   ProjectEuler.py
    	modified:   __init__.py

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -2,19 +2,21 @@
 # _*_ coding: UTF-8 _*_
 # File          :  pe.py
 # Created       :  Mon 27 Apr 2015 19:01:22
-# Last Modified :  Sat 23 May 2015 00:05:45
+# Last Modified :  Sat 30 May 2015 23:34:56
 # Maintainer    :  sharlatan, <ssharlatanus@gmail.com>
 # Title         :  PROJECT EULER FUNCTIONS
 # License       :  Same as Python (GPL)
 # Credits       :  https://projecteuler.net
 #
-# -=:[ Description ]:=-
+# -=:[ DESCRIPTION ]:=-
 # This file contains functions which I used for solving Project Euler's
 # problems.
 # num is alway a integer number given as argument to any of functions.
+# <END OF DESCRIPTION>---------------------------------------------------------
+
 
 def fnls():
-    """ Return a list of used functions. """
+    """ Return a list of used functions with short description. """
     function_list = {
         "isp(num)"    : "Check num for primality.",
         "ps(num)"     : "Yield prime sieve up to num.",
@@ -30,8 +32,6 @@ def fnls():
                     }
     for key in function_list:
         print key,"\t:", function_list[key]
-
-
 # <END  OF  DESCRIPTION>-------------------------------------------------------
 
 
@@ -40,7 +40,6 @@ import itertools
 
 
 # -=:[ Prime numbers ]:=-
-
 def isp(num):
     """ Is prime?: check num for primality. """
     if num == 1:
@@ -72,13 +71,11 @@ def ps(n):
         if isp(i):
             yield i
         i += 1
-
-
 # <END OF PRIME NUMBERS>--------------------------------------------------------
 
-# -=:[ Divisors ]:=-
 
-def dr(ran, num):
+# -=:[ Divisors ]:=-
+def divr(ran, num):
     """ Divisor range: generate a list of number in ran dividable by num. """
     r = 2
     while r < ran:
@@ -87,15 +84,22 @@ def dr(ran, num):
         r += 1
 
 
-# <END OF # DIVISORS>-----------------------------------------------------------
-
-
 def divs(num):
-    """ Yield list of proper divisor of num. """
+    """ Yield unsorted list of proper divisor of num. """
     for d in xrange(1, int(math.sqrt(num))):
         if num % d == 0:
             yield d
             yield num // d
+
+
+def divp(num):
+    ''' Prime divisors: generate range of prime divi for num. '''
+    for d in xrange(2, int(math.sqrt(num))):
+        if isp(d) and num % d:
+            yield d
+# <END OF  DIVISORS>------------------------------------------------------------
+
+
 
 
 def ds(num):
@@ -128,33 +132,71 @@ def pclrow(n):
 
 
 # -=:[ Figurate numbers ]:=-
-def trgn(n):
-    """ Yield a list of ingeter triangular numbers up to num. """
-    for tr in xrange(0, n+1):
-        yield tr*(tr+1)/2
-    # -=:[ Reference ]:=-
-    # Carl Friedrich Gauss
-    # OEIS : A000217
-    # -=:[ Links ]:=-
-    # https://en.wikipedia.org/wiki/Triangular_number
+def trgn(n, only_one=None):
+    """ Triangular numbers:  generate a list of ingeter trgn  up to num. """
+    if only_one == "one":
+        yield n * (n + 1) / 2
+    else:
+        for tr in xrange(0, n+1):
+            yield tr*(tr+1)/2
+
+
+def sqrn(n, only_one=None):
+    ''' Square number[s]: generate list of sqrn if not given "one". ''' 
+    if only_one == "one":
+        yield n**2
+    else:
+        for sq in xrange(0, n+1):
+            yield sq**2
+
+
+def pntn(n, only_one=None):
+    ''' Pentagonal number[s]: yeild list if not "one".'''
+    if only_one == "one":
+        yield (3 * n**2 - n) / 2
+    else:
+        for pen in xrange(0, num+1):
+            yield (3 * pen**2 - pen) / 2
+
+
+def hexn(n, only_one=None):
+    ''' Hexagonal number: generated up to n's position. '''
+    if only_one == "one":
+        yield n * (2*n - 1)
+    else:
+        for h in xrange(0, n+1):
+            yield h * (2*h - 1)
+
+
+def hepn(n, only_one=None):
+    ''' Heptagoanl number: generated up to n's position. '''
+    if only_one == "one":
+        yield  n * (5*n - 3) / 2
+    else:
+        for hep in xrange(0, n+1):
+            yield n * (5*n - 3) / 2
+
+
+def octn(n, only_one=None):
+    ''' Octagoanl number: generated up to n's position. '''
+    if only_one == "one":
+        yield  n * (3*n + 2)
+    else:
+        for o in xrange(0, n+1):
+            yield o * (3*o + 2)
 
 
 def ttrn(n):
     """ Yield a list of integer tetrahedral numbers up to n's position. """
     for n in xrange(0, n+1):
         yield (n*(n + 1) * (n + 2)) / 6
-
-
-def sqrn(n):
-    """ Yield a list of nterger square number up to num's position. """
-    for sq in xrange(0, n+1):
-        yield sq**2
-
-
-def pntn(num):
-    """ Yield a list of integer pentagoanl numbers up to num's position. """
-    for pen in xrange(0, num+1):
-        yield (3 * pen**2 - pen) / 2
-    # -=:[ References ]:=-
-    # Euler's theory of partions
 # <END OF FIGURATE NUBERS>-----------------------------------------------------
+
+
+# -=:[ Reference ]:=-
+# Carl Friedrich Gauss
+# Euler's theory of partions
+# OEIS : A000217
+
+# -=:[ Links ]:=-
+# https://en.wikipedia.org/wiki/Triangular_number
---
+++ __init__.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# _*_ coding: UTF-8 _*_
+# File          :  __init__.py
+# Created       :  Sat 30 May 2015 22:25:33
+# Last Modified :  Sat 30 May 2015 22:42:22
+# Maintainer    :  sharlatan, <sharlatanus@gmail.com>
+# Title         :
+# License       :  Same as Python (GPL)
+# Credits       :
+#
+# -=:[ Description ]:=-
+#
+# 
+# <END  OF  DESCRIPTION>-------------------------------------------------------
+
+__all__ = [ "isp(num)", "ps(num)", "divs(num)", "ds(num)", 
+            "dfs(num)", "dsfs(num)", "pclrow(num)", "trgn(num)",
+            "ttrn(n)", "sqrn(n)", "pntn(num)" ]
