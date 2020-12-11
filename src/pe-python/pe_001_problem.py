42beae49c55f1b0c077ccabbec4f52d3867f4ae8
Oleg Bocharov <oleg.bocharov@mirrorweb.com>
2020-12-11 20:58:45 +0000

    rename Python files

--- src/pe-python/pe_001_problem.py
+++ src/pe-python/pe_001_problem.py
@@ -14,9 +14,8 @@
 #
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-q
 sum_mult = 0
-for i in xrange(1, 100000000):
+for i in range(1, 1000):
     if i % 3 == 0 or i % 5 == 0:
         sum_mult += i
-print sum_mult
+print(sum_mult)
--- src/pe-python/pe_009_problem.py
+++ src/pe-python/pe_009_problem.py
@@ -7,16 +7,18 @@
 # Title         :  SPECIAL PYTHAGOREAN TRIPLET
 # License       :  Same as Python (GPL)
 # Credits       :  https://projecteuler.net
-#
-# -=:[ Description ]:=-
-# Special Pythagorean triplet
-# A Pythagorean triplet is a set of three natural numbers, a < b < c, for wich,
-#
-#     a**2 + b**2 = c**2
-#
-# For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2
-# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-# Find the product abc.
+
+'''
+-=:[ Description ]:=-
+Special Pythagorean triplet
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for wich,
+
+    a**2 + b**2 = c**2
+
+For example, 3**2 + 4**2 = 9 + 16 = 25 = 5**2
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+'''
 
 
 from termcolor import colored
@@ -47,7 +49,7 @@ def pythanumber(s):
 
 
 def error():
-    """ Massage if error """
+    ''' Massage if error '''
 
     print colored("..::Incorrect amount of arguments::..", 'red')
     print colored("\tEnter just one integer", 'blue')
@@ -55,7 +57,7 @@ def error():
 
 
 def perf():
-    # Return performance time of the program
+    '''Return performance time of the program'''
     return ("Performance time: %s" % (time.time() - INIT_TIME))
 
 
